cd55c7480d0c3c5f70ae69f218145a09b30da0fa
renebec <39104197+renebec@users.noreply.github.com>
2024-02-03 15:45:08 -0600

    Add files via upload

--- docs/actions.js
+++ docs/actions.js
@@ -1,7 +1,3 @@
-var var scale = window.outerWidth / 1280;
-
-$('head').append('<meta name="viewport" content="width=1280, initial-scale=' + scale + ', maximum-scale=' + scale + ', user-scalable=0">');
-
 
 
 function myFunction0() {
